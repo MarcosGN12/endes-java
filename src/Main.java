4fbd6a4d0339ade1ad44a9462a73f1862f9ac934
marcos <marcosnuero11@gmail.com>
2024-04-28 15:04:49 +0200

    cambio4

---
+++ src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        System.out.println("CAMBIOS");
+        System.out.println("CAMBIO1");
+        do {
+            System.out.println("Menú de Calculadora:");
+            System.out.println("1. Suma");
+            System.out.println("2. Resta");
+            System.out.println("3. Multiplicación");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    realizarSuma(scanner);
+                    break;
+                case 2:
+                    realizarResta(scanner);
+                    break;
+                case 3:
+                    realizarMultiplicacion(scanner);
+                    break;
+                case 4:
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+                    break;
+            }
+        } while (opcion != 4);
+
+        scanner.close();
+    }
+
+    public static void realizarSuma(Scanner scanner) {
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+        double resultado = num1 + num2;
+        System.out.println("La suma es: " + resultado);
+    }
+
+    public static void realizarResta(Scanner scanner) {
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+        double resultado = num1 - num2;
+        System.out.println("La resta es: " + resultado);
+    }
+
+    public static void realizarMultiplicacion(Scanner scanner) {
+        System.out.print("Ingrese el primer número: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Ingrese el segundo número: ");
+        double num2 = scanner.nextDouble();
+        double resultado = num1 * num2;
+        System.out.println("La multiplicación es: " + resultado);
+    }
+}
